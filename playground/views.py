9cf07aa74303a527dcd6de3de339f6bfa17ec239
AnSeoyeon <s2227@e-mirim.hs.kr>
2024-03-25 12:16:33 +0900

    views -> templates에 context 전달

--- playground/views.py
+++ playground/views.py
@@ -5,7 +5,12 @@ def say_hello(request):
     return HttpResponse("Hello World")
 
 def say_hello_html(request):
-    return render(request, 'playground/hello.html')
+    return render(request, 'playground/hello.html', {'name': '지우야',
+                                                     'greeting': '안녕~'})
 
 def say_bye_html(request):
-    return render(request, 'playground/bye.html')
+    context = {
+        'singer': 'WOODZ',
+        'title': 'Drowning',
+    }
+    return render(request, 'playground/bye.html', context = context)
