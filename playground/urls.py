188e7299595ea6cc8f387827a75ad59244bc9959
AnSeoyeon <s2227@e-mirim.hs.kr>
2024-03-25 12:06:59 +0900

    views, templates, urls: playground/ -> bye_html/ -> say_bye_html() -> bye.html

--- playground/urls.py
+++ playground/urls.py
@@ -7,4 +7,6 @@ app_name = 'playground'
 urlpatterns = {
     path('hello/', views.say_hello, name='hello'),  # {% url 'playground:hello' %}
     path('hello_html/', views.say_hello_html, name='hello_html'),  # {% url 'playground:hello_html' %}
+    path('bye_html/', views.say_bye_html, name='bye_html'),  # {% url 'playground:hello_html' %}
+
 }
